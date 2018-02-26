de3da9c5a11dda5a51395d73e4e0eb90bc364a51
bluefire8421 <sbeaver57@yahoo.com>
2018-02-26 14:44:26 -0600

    Create generate_words.cpp

---
+++ assignments/program_3/generate_words.cpp
@@ -0,0 +1,150 @@
+//Name: Sarah Beaver
+//Date: February 26, 2018
+//Assignment : Program 3
+//	Description :
+//	  creating crazy words from adverbs, adjectives, animals, nouns, and verbs
+#include<iostream>
+#include <fstream>
+#include <time.h>
+#include <string>
+#include<vector>
+#include <stdlib.h>     
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	ifstream animal;
+	animal.open("animal.txt");
+	ifstream adverb;
+	adverb.open("adverb.txt");
+	ifstream adjective;
+	adjective.open("adjectives.txt");
+	ifstream noun, verb;
+	noun.open("noun.txt");
+	verb.open("verb.txt");
+	ofstream output;
+	output.open("tenthousandwords.txt");
+	vector<string>animallist;
+	vector<string>adverblist;
+	vector<string>adjectivelist;
+	vector<string>nounlist;
+	vector<string>verblist;
+	string s;
+	srand(57);
+
+	//inserting the words into apprpiate lists
+	while (animal.good())
+	{
+		animal >> s;
+		animallist.push_back(s);
+	}
+	while (adverb.good())
+	{
+		adverb >> s;
+		adverblist.push_back(s);
+	}
+	while (adjective.good())
+	{
+		adjective >> s;
+		adjectivelist.push_back(s);
+	}
+	while (noun.good())
+	{
+		noun >> s;
+		nounlist.push_back(s);
+	}
+	while (verb.good())
+	{
+		verb >> s;
+		verblist.push_back(s);
+	}
+
+	//creating 10000 crazy words
+	for (int i = 0; i < 10000; i++)
+	{
+		//7 is number of different random possibilities
+		int j = rand() % 7;
+		int spot;
+		//adv adj animal
+		if (j==1)
+		{
+			
+			spot = rand() % adverblist.size();
+			s += adverblist[spot] + " ";
+			spot = rand() % adjectivelist.size();
+			s += adjectivelist[spot] + " ";
+			spot = rand() % animallist.size();
+			s += animallist[spot] + " ";
+		}
+		// adj verb adv noun
+		else if (j == 2)
+		{
+			spot = rand() % adjectivelist.size();
+			s += adjectivelist[spot] + " ";
+			spot = rand() % verblist.size();
+			s += verblist[spot] + " ";
+			spot = rand() % adverblist.size();
+			s += adverblist[spot] + " ";
+			spot = rand() % nounlist.size();
+			s += nounlist[spot] + " ";
+		}
+		//adjective adjective noun
+		else if (j == 3)
+		{
+			spot = rand() % adjectivelist.size();
+			s += adjectivelist[spot] + " ";
+			spot = rand() % adjectivelist.size();
+			s += adjectivelist[spot] + " ";
+			spot = rand() % nounlist.size();
+			s += nounlist[spot] + " ";
+		}
+		//noun adjective noun
+		else if (j == 4)
+		{
+			spot = rand() % nounlist.size();
+			s += nounlist[spot] + " ";
+			spot = rand() % adjectivelist.size();
+			s += adjectivelist[spot] + " ";
+			spot = rand() % nounlist.size();
+			s += nounlist[spot] + " ";
+		}
+		//adv verb animal
+		else if (j == 5)
+		{
+			spot = rand() % adverblist.size();
+			s += adverblist[spot] + " ";
+			spot = rand() % verblist.size();
+			s += verblist[spot] + " ";
+			spot = rand() % animallist.size();
+			s += animallist[spot] + " ";
+		}
+		//adv adv verb animal
+		else if(j==6)
+		{
+			spot = rand() % adverblist.size();
+			s += adverblist[spot] + " ";
+			spot = rand() % adverblist.size();
+			s += adverblist[spot] + " ";
+			spot = rand() % verblist.size();
+			s += verblist[spot] + " ";
+			spot = rand() % animallist.size();
+			s += animallist[spot] + " ";
+		}
+		//adv adj verb animal
+		else 
+		{
+			spot = rand() % adverblist.size();
+			s += adverblist[spot] + " ";
+			spot = rand() % adjectivelist.size();
+			s += adjectivelist[spot] + " ";
+			spot = rand() % verblist.size();
+			s += verblist[spot] + " ";
+			spot = rand() % animallist.size();
+			s += animallist[spot] + " ";
+		}
+		output <<s<<"\n";
+		s = "";
+	}
+	system("pause");
+}
